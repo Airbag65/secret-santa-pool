dd9f79554fc0fcbde2a621009a412fe9f72ddab7
Anton Norman <normananton03@gmail.com>
2025-01-02 10:36:30 +0100

    getting new uuid from created pool and navigating to pool with uuid param

--- client/index.js
+++ client/index.js
@@ -1,17 +1,18 @@
 const createPool = async () => {
     try {
-        // const res = await fetch("http://127.0.0.1:8080")
-        // console.log(res.ok)
         const res = await fetch("http://127.0.0.1:8080/createpool", {
             method: 'POST'
         }) 
         if(!res.ok){
             console.log(res.status)
-        }
+            return
+        }         
         const json = await res.json() 
-        console.log(json)
+        console.log(json.uuid)
+        window.location.href = `./pool.html?uid=${json.uuid}`
 
     } catch (error) {
         console.log(error)
+        return
     }
 }
